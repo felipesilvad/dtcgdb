bcc0d5ec7cdd892d846017a7e0a73c80eb5b8a03
Felipe <felipe.silvad@outlook.com>
2020-05-20 01:10:50 -0300

    Update urls.py

--- dtcg/urls.py
+++ dtcg/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admdtcg/', admin.site.urls),
     path('', views.home, name='home'),
     path('cards/', include('cards.urls')),
 ]
