332259a863d7060b00ef2655d0da85a3b36083cc
felipesilvad <31076991+felipesilvad@users.noreply.github.com>
2021-03-20 16:57:41 -0300

    idk

--- prices/prices.py
+++ prices/prices.py
@@ -1,48 +1,48 @@
-import requests
-from cards.models import Card
-import lxml
-import gspread
-import schedule
-import time
-import datetime
-from bs4 import BeautifulSoup
-from forex_python.converter import CurrencyRates
+# import requests
+# from cards.models import Card
+# import lxml
+# import gspread
+# import schedule
+# import time
+# import datetime
+# from bs4 import BeautifulSoup
+# from forex_python.converter import CurrencyRates
 
-c = CurrencyRates()
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
+# c = CurrencyRates()
+# headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
 
-cards = Card.objects.all().order_by('number').filter(set__set_type="Booster")
+# cards = Card.objects.all().order_by('number').filter(set__set_type="Booster")
 
-def request(URL_suruga_ya):
-    if URL_suruga_ya == None:
-        price_suruga_ya = None
-    else:
-        page_suruga_ya = requests.get(URL_suruga_ya, headers=headers)
-        soup_suruga_ya = BeautifulSoup(page_suruga_ya.content, 'lxml')
-        price_suruga_ya = soup_suruga_ya.find(class_="text-price-detail price-buy").text
-    return price_suruga_ya
+# def request(URL_suruga_ya):
+#     if URL_suruga_ya == None:
+#         price_suruga_ya = None
+#     else:
+#         page_suruga_ya = requests.get(URL_suruga_ya, headers=headers)
+#         soup_suruga_ya = BeautifulSoup(page_suruga_ya.content, 'lxml')
+#         price_suruga_ya = soup_suruga_ya.find(class_="text-price-detail price-buy").text
+#     return price_suruga_ya
 
-def parse(price_suruga_ya):
-    if price_suruga_ya == None:
-        date = datetime.datetime.today()
-        jpy_suruga_ya = None
-        usd_suruga_ya = None
-        values = {'date':date, 'jpy_suruga_ya': jpy_suruga_ya, 'usd_suruga_ya':usd_suruga_ya}
-    else:
-        jpy_suruga_ya = int(price_suruga_ya.replace('円 (税込)', ''))
-        usd_suruga_ya = round((c.convert('JPY', 'USD', jpy_suruga_ya)), 2)
-        date = datetime.datetime.today()
-        values = {'date':date, 'jpy_suruga_ya': jpy_suruga_ya, 'usd_suruga_ya':usd_suruga_ya}
-    return values
+# def parse(price_suruga_ya):
+#     if price_suruga_ya == None:
+#         date = datetime.datetime.today()
+#         jpy_suruga_ya = None
+#         usd_suruga_ya = None
+#         values = {'date':date, 'jpy_suruga_ya': jpy_suruga_ya, 'usd_suruga_ya':usd_suruga_ya}
+#     else:
+#         jpy_suruga_ya = int(price_suruga_ya.replace('円 (税込)', ''))
+#         usd_suruga_ya = round((c.convert('JPY', 'USD', jpy_suruga_ya)), 2)
+#         date = datetime.datetime.today()
+#         values = {'date':date, 'jpy_suruga_ya': jpy_suruga_ya, 'usd_suruga_ya':usd_suruga_ya}
+#     return values
 
-def output(values, i):
-    gs = gspread.service_account(filename='prices\creds.json')
-    sh = gs.open('DTCGprices').sheet1
-    # sh.update("A2", [[card.number, card.title, str(values['date']), values['jpy_suruga_ya'], values['usd_suruga_ya']]])
-    sh.update_cell(i, 1, cards[i].number)
-    sh.update_cell(i, 2, cards[i].title)
-    if values['jpy_suruga_ya'] == None:
-        sh.update_cell(i, 3, "-")
-    else:
-        sh.update_cell(i, 3, values['jpy_suruga_ya'])
-    return
+# def output(values, i):
+#     gs = gspread.service_account(filename='prices\creds.json')
+#     sh = gs.open('DTCGprices').sheet1
+#     # sh.update("A2", [[card.number, card.title, str(values['date']), values['jpy_suruga_ya'], values['usd_suruga_ya']]])
+#     sh.update_cell(i, 1, cards[i].number)
+#     sh.update_cell(i, 2, cards[i].title)
+#     if values['jpy_suruga_ya'] == None:
+#         sh.update_cell(i, 3, "-")
+#     else:
+#         sh.update_cell(i, 3, values['jpy_suruga_ya'])
+#     return
--- prices/updater.py
+++ prices/updater.py
@@ -1,11 +1,12 @@
-from datetime import datetime
-from prices import prices
-from cards.models import Card
+# from datetime import datetime
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from prices import prices
+# from cards.models import Card
 
-cards = Card.objects.all().order_by('number').filter(set__set_type="Booster")
+# cards = Card.objects.all().order_by('number').filter(set__set_type="Booster")
 
-def start():
-    for i in range(1, len(cards)):
-        data = prices.request(cards[i].suruga_ya)
-        values = prices.parse(data)
-        # prices.output(values, i)
+# def start():
+#     for i in range(1, len(cards)):
+#         data = prices.request(cards[i].suruga_ya)
+#         values = prices.parse(data)
+#         # prices.output(values, i)
